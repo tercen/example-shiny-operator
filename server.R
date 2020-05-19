2abcc2d1a4fd018ecb6657abe3f3022ad87bd0f1
agouy <alexandre.gouy@gmail.com>
2020-05-19 16:21:54 +0000

    use of pheatmap package

--- server.R
+++ server.R
@@ -2,40 +2,47 @@ library(shiny)
 library(tercen)
 library(dplyr)
 library(tidyr)
+library(pheatmap)
 
 shinyServer(function(input, output, session) {
   
-  dataInput = reactive({getValues(session)})
+  dataInput <- reactive({
+    getValues(session)
+  })
+  
+  output$reacOut <- renderUI({
+    plotOutput(
+      "heatmap",
+      height = input$plotHeight,
+      width = input$plotWidth
+    )
+  }) 
   
   output$heatmap <- renderPlot({
-    
     data <- dataInput()
-    data_wide <- spread(data, .ci, .y)[, -1]
-
-    heatmap(as.matrix(data_wide))
+    data_wide <- as.matrix(spread(data, .ci, .y)[, -1])
+    
+    rownames(data_wide) <- ctx$rselect()[[1]]
+    colnames(data_wide) <-  ctx$cselect()[[1]]    
+    pheatmap(data_wide, fontsize_row = input$rowFontSize)
     
   })
-
+  
 })
 
-
-
-getCtx = function(session){
+getCtx <- function(session){
   # retreive url query parameters provided by tercen
-  query = parseQueryString(session$clientData$url_search)
-  token = query[["token"]]
-  taskId = query[["taskId"]]
-
+  query <- parseQueryString(session$clientData$url_search)
+  token <- query[["token"]]
+  taskId <- query[["taskId"]]
+  
   # create a Tercen context object using the token
-  ctx = tercenCtx(taskId = taskId, authToken = token)
-  
-  # ctx = tercenCtx(workflowId = "3b732c1081004d8ad810464ee10076d9", stepId =  "0d5c2e44-ad52-4382-b60c-f745382bd47c")
-  
+  ctx <- tercenCtx(taskId = taskId, authToken = token)
   return(ctx)
 }
 
-getValues = function(session){
-  ctx = getCtx(session)
-  data = ctx %>% select(.y , .ci , .ri)
+getValues <- function(session){
+  ctx <- getCtx(session)
+  data <- ctx %>% select(.y , .ri, .ci)
   return(data)
 }
--- ui.R
+++ ui.R
@@ -1,6 +1,17 @@
 library(shiny)
 
 shinyUI(fluidPage(
+  
   titlePanel("Heatmap"),
-  mainPanel(plotOutput("heatmap"))
+  
+  sidebarPanel(
+    sliderInput("plotWidth", "Plot width (px)", 200, 2000, 500),
+    sliderInput("plotHeight", "Plot width (px)", 200, 2000, 1200),
+    sliderInput("rowFontSize", "Row labels font size", 5, 20, 7, step = 1)
+  ),
+  
+  mainPanel(
+    uiOutput("reacOut")
+  )
+  
 ))
