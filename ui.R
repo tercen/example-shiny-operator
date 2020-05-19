7d123150bf5603264003ddbbd55d63981144ce72
agouy <alexandre.gouy@gmail.com>
2020-05-19 14:44:08 +0000

    first commit

--- ui.R
+++ ui.R
@@ -1,11 +1,8 @@
 library(shiny)
 
 shinyUI(fluidPage(
-
   # Application title
-  titlePanel("Tercen shiny operator"),
- 
-  mainPanel(
-    textOutput("status")
-  )
-))
+  titlePanel("Box Plot"),
+  mainPanel(plotOutput("distPlot"))
+)
+)
