5bdb62e46103d73a6b57fd4ac617c7cc9568ac4f
agouy <alexandre.gouy@gmail.com>
2020-05-19 14:58:32 +0000

    removed d3

--- server.R
+++ server.R
@@ -1,9 +1,6 @@
 library(shiny)
 library(tercen)
 library(dplyr)
-library(d3heatmap)
-library(tidyr)
-
 
 shinyServer(function(input, output, session) {
   
@@ -13,10 +10,8 @@ shinyServer(function(input, output, session) {
     
     data <- dataInput()
     data_wide <- spread(data, .ci, .y)[, -1]
-    print(data_wide)
-    str(data_wide)
-    
-    d3heatmap(data_wide)
+
+    heatmap(as.matrix(data_wide))
     
   })
 
@@ -31,14 +26,12 @@ getCtx = function(session){
   taskId = query[["taskId"]]
 
   # create a Tercen context object using the token
-  ctx = tercenCtx(taskId = taskId, authToken = token)
+  # ctx = tercenCtx(taskId = taskId, authToken = token)
   
-  # ctx = tercenCtx(workflowId = "3b732c1081004d8ad810464ee10076d9", stepId =  "0d5c2e44-ad52-4382-b60c-f745382bd47c")
+  ctx = tercenCtx(workflowId = "3b732c1081004d8ad810464ee10076d9", stepId =  "0d5c2e44-ad52-4382-b60c-f745382bd47c")
   
   return(ctx)
 }
-
-# http://localhost:5402/admin/w/3b732c1081004d8ad810464ee10076d9/ds/0d5c2e44-ad52-4382-b60c-f745382bd47c
 
 getValues = function(session){
   ctx = getCtx(session)
