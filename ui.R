52874ff9704cdf24ea8dab6c0afa718d89b93986
ginberg <ginberg@gmail.com>
2021-05-27 10:46:17 +0200

    prevent shiny timeout

--- ui.R
+++ ui.R
@@ -1,6 +1,10 @@
 library(shiny)
+library(shinyjs)
 
 shinyUI(fluidPage(
+  shinyjs::useShinyjs(),
+  tags$script(HTML('setInterval(function(){ $("#hiddenButton").click(); }, 1000*30);')),
+  tags$footer(shinyjs::hidden(actionButton(inputId = "hiddenButton", label = "hidden"))),
   
   titlePanel("Histogram"),
   
